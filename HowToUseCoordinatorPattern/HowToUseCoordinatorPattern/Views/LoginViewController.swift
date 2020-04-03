0bb0281dc317da4559649d02946ff6d93fc0c7c7
Daniel Gx <danielgx@MacBook-Pro-de-ioasys.local>
2020-04-03 18:39:56 -0300

    pushing coordinator example

---
+++ HowToUseCoordinatorPattern/HowToUseCoordinatorPattern/Views/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  HowToUseCoordinatorPattern
+//
+//  Created by Daniel Gx on 01/04/20.
+//  Copyright © 2020 Daniel Gx. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: - Variables
+    
+    // MARK: - Outlets
+    
+    // MARK: - Life Cycle
+    
+    // MARK: - Funcs
+}
